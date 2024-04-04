bfc72e857e7a6ad71f2906ecdc82e518417c168b
syawaliaaaz <helfinasyawaliaaa@gmail.com>
2024-04-04 11:40:02 +0700

    Membuat cout dan cin didalam looping for

--- search_lia/search_lia.cpp
+++ search_lia/search_lia.cpp
@@ -17,11 +17,15 @@ void input()
 			cout << "\nArray should have minimum 1 and maximum 20 elements. \n\n";
 }
 		
-cout << "\n-----------------------\n";
-cout << " Enter array elements \n";
-cout << "--------------------- \n";
-for (i = 0; i < n; i++)
-{
+    cout << "\n-----------------------\n";
+    cout << " Enter array elements \n";
+    cout << "--------------------- \n";
+    for (i = 0; i < n; i++)
+    {
+	     cout << "<" << (i + 1) << "> ";
+	     cin >> arr[i];
 
+    }
 }
+
 
